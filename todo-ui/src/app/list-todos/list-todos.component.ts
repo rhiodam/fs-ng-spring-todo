5c89ac0e343b98d9d57e1574f749ded68d3aa55b
rhiodam <rhioda.m@gmail.com>
2019-03-24 02:45:58 +0700

    step 22 todo class for displaying data

--- todo-ui/src/app/list-todos/list-todos.component.ts
+++ todo-ui/src/app/list-todos/list-todos.component.ts
@@ -1,5 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
+
+export class Todo {
+  constructor(public id: number,
+              public description: string,
+              public done: boolean,
+              public targetDate: Date) {
+
+
+  }
+}
 @Component({
   selector: 'app-list-todos',
   templateUrl: './list-todos.component.html',
@@ -8,20 +18,26 @@ import { Component, OnInit } from '@angular/core';
 export class ListTodosComponent implements OnInit {
 
   todos = [
+    new Todo(1, 'Learn Angular', false, new Date()),
+    new Todo(2, 'Learn Angular 2', false, new Date()),
+    new Todo(3, 'Learn Angular 3', false, new Date()),
+    new Todo(4, 'Learn Angular 4', false, new Date())
+  ]
+  todos2 = [
     {
-      id : 1,
+      id: 1,
       description: 'Learn Angular'
     },
     {
-      id : 2,
+      id: 2,
       description: 'Learn React'
     },
     {
-      id : 3,
+      id: 3,
       description: 'Learn Vue'
     },
     {
-      id : 4,
+      id: 4,
       description: 'Learn Backbone'
     }
   ]
@@ -31,7 +47,8 @@ export class ListTodosComponent implements OnInit {
   //   description: 'Learn Angular'
   // }
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
