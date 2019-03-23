1b55eaa87eb05d454bc49ad7bd9ebca42da4f776
rhiodam <rhioda.m@gmail.com>
2019-03-23 22:58:57 +0700

    step 13

--- todo-ui/src/app/login/login.component.ts
+++ todo-ui/src/app/login/login.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  username = 'rhioda';
+  password = '';
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  handleLogin() {
+    console.log(this.username + this.password);
+  }
 }
