3e39b3859f8fe729bd321054240379ca3b50729f
rhiodam <rhioda.m@gmail.com>
2019-03-24 05:01:30 +0700

    Update hardcoded-authentication.service.ts

--- todo-ui/src/app/service/hardcoded-authentication.service.ts
+++ todo-ui/src/app/service/hardcoded-authentication.service.ts
@@ -8,10 +8,18 @@ export class HardcodedAuthenticationService {
   constructor() { }
 
   authenticate(username , password){
+    console.log('before auth :' + this.isUserLoggedIn());
     if (username === 'rhioda' && password === 'muthie') {
+      sessionStorage.setItem('authenticatedUser' , username);
+      console.log('after auth :' + this.isUserLoggedIn());
       return true;
     }
       return false;
 
+  }
+
+  isUserLoggedIn(){
+    let user = sessionStorage.getItem('authenticatedUser');
+    return !(user === null)
   }
 }
