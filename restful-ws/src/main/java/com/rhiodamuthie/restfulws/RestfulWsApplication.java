08b4ce9b35b9a3c5a6e4032cbb438b098cb195df
rhiodam <rhioda.m@gmail.com>
2019-03-24 07:35:31 +0700

    Add

---
+++ restful-ws/src/main/java/com/rhiodamuthie/restfulws/RestfulWsApplication.java
@@ -0,0 +1,13 @@
+package com.rhiodamuthie.restfulws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulWsApplication.class, args);
+	}
+
+}
---
+++ restful-ws/src/test/java/com/rhiodamuthie/restfulws/RestfulWsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rhiodamuthie.restfulws;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RestfulWsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
