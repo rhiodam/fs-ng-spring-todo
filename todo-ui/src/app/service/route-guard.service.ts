e7c84b0e7958564f391612846a2ec7529cef4720
rhiodam <rhioda.m@gmail.com>
2019-03-27 04:23:38 +0700

    Update route-guard.service.ts

--- todo-ui/src/app/service/route-guard.service.ts
+++ todo-ui/src/app/service/route-guard.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import {CanActivate, RouterStateSnapshot, ActivatedRouteSnapshot, Router} from "@angular/router";
 import {HardcodedAuthenticationService} from "./hardcoded-authentication.service";
+import {BasicAuthenticationService} from "./basic-authentication.service";
 
 @Injectable({
   providedIn: 'root'
@@ -9,12 +10,14 @@ export class RouteGuardService implements CanActivate{
 
   constructor(
     private hardcodedAuthenticationService : HardcodedAuthenticationService,
+    private basicAuthenticationService : BasicAuthenticationService,
     private router: Router
 
   ) { }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot){
-    if (this.hardcodedAuthenticationService.isUserLoggedIn())
+    // if (this.hardcodedAuthenticationService.isUserLoggedIn())
+    if (this.basicAuthenticationService.isUserLoggedIn())
       return true;
 
     // if not logged in
