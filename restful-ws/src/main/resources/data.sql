2314924197d8c023401396ce0510b65f919f56e4
rhiodam <rhioda.m@gmail.com>
2019-03-27 03:24:02 +0700

    Update data.sql

--- restful-ws/src/main/resources/data.sql
+++ restful-ws/src/main/resources/data.sql
@@ -1,7 +1,7 @@
-                        `id` ,	`description` `is_done` `target_date` 	`username`
+--                         `id` ,	`description` `is_done` `target_date` 	`username`
 
- insert into Todo values (1 ,  'Learn Angular' , null , SYSDATE  , 'rhioda');
- insert into Todo values (2 ,  'Learn Angular' , null , SYSDATE  , 'rhioda');
- insert into Todo values (3 ,  'Learn Angular' , null , SYSDATE  , 'rhioda');
- insert into Todo values (4 ,  'Learn Angular' , null , SYSDATE  , 'rhioda');
- insert into Todo values (5 ,  'Learn Angular' , null , SYSDATE  , 'rhioda');
+ insert into Todo values (1 ,  'Learn Angular' , 0 , SYSDATE  , 'rhioda');
+ insert into Todo values (2 ,  'Learn Angular' , 0 , SYSDATE  , 'rhioda');
+ insert into Todo values (3 ,  'Learn Angular' , 0 , SYSDATE  , 'rhioda');
+ insert into Todo values (4 ,  'Learn Angular' , 0 , SYSDATE  , 'rhioda');
+ insert into Todo values (5 ,  'Learn Angular' , 0 , SYSDATE  , 'rhioda');
