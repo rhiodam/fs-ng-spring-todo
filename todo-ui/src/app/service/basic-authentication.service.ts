0e8ae5f2034da123414d426f608666fc161a71e8
rhiodam <rhioda.m@gmail.com>
2019-04-01 01:53:51 +0700

    FATAL Typo jadi si auth ny malah pake username ckckck

--- todo-ui/src/app/service/basic-authentication.service.ts
+++ todo-ui/src/app/service/basic-authentication.service.ts
@@ -2,20 +2,19 @@ import {Injectable} from '@angular/core';
 import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {HelloWorldBean} from "./data/welcome-data.service";
 import {map} from "rxjs/operators";
+import {API_URL} from "../app.constants";
 
 @Injectable({
   providedIn: 'root'
 })
 export class BasicAuthenticationService {
 
-  constructor(
-    private http: HttpClient
-  ) {
+  constructor(private http:HttpClient) {
   }
 
   excuteBasicAuthService(username, password) {
     let basicAuthHeaderString = 'Basic ' + window.btoa(username + ':' + password);
-    console.log(username + ' ' + password);
+    console.log(username + ' ' + password + ' : ' + basicAuthHeaderString);
     // let header: HttpHeaders;
 
     let header = new HttpHeaders({
@@ -23,7 +22,7 @@ export class BasicAuthenticationService {
     });
 
     return this.http
-      .get<Auth>(`http://localhost:8080/basicauth`,
+      .get<Auth>(`${API_URL}/basicauth`,
         {headers: header})
       .pipe(
         map(
@@ -34,6 +33,27 @@ export class BasicAuthenticationService {
           }
         )
       );
+  }
+
+
+  isUserLoggedIn() {
+    let user = sessionStorage.getItem('authenticatedUser');
+    return !(user === null)
+  }
+
+  getAuthenticatedUser() {
+    return sessionStorage.getItem('authenticatedUser');
+  }
+
+  getAuthenticatedToken() {
+    if (this.getAuthenticatedUser()) {
+      return sessionStorage.getItem('token');
+    }
+  }
+
+  logout() {
+    sessionStorage.removeItem('authenticatedUser')
+    sessionStorage.removeItem('token')
   }
 
   // createBasicAuthHttpHeader() {
@@ -54,31 +74,9 @@ export class BasicAuthenticationService {
   //
   // }
 
-  isUserLoggedIn() {
-    let user = sessionStorage.getItem('authenticatedUser');
-    return !(user === null)
-  }
-
-  getAuthenticatedUser() {
-    return sessionStorage.getItem('authenticatedUser');
-  }
-
-  getAuthenticatedToken() {
-    if (this.getAuthenticatedUser()) {
-      return sessionStorage.getItem('authenticatedUser');
-    }
-
-  }
-
-  logout() {
-    sessionStorage.removeItem('authenticatedUser')
-    sessionStorage.removeItem('token')
-  }
 }
 
 export class Auth {
-  constructor(
-    public message: string
-  ) {
+  constructor(public message:string) {
   }
 }
