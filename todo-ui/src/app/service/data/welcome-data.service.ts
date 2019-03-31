1a57d02815f5366fbb60c50c2fb7011100d2ebe0
rhiodam <rhioda.m@gmail.com>
2019-04-01 02:02:24 +0700

    step 83 done

--- todo-ui/src/app/service/data/welcome-data.service.ts
+++ todo-ui/src/app/service/data/welcome-data.service.ts
@@ -1,5 +1,6 @@
 import {Injectable} from '@angular/core';
 import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {API_URL} from "../../app.constants";
 
 @Injectable({
   providedIn: 'root'
@@ -12,37 +13,39 @@ export class WelcomeDataService {
   }
 
   excuteHelloWorldBeanService() {
-    return this.http.get<HelloWorldBean>('http://localhost:8080/hello-world-bean')
+    return this.http.get<HelloWorldBean>(`${API_URL}/hello-world-bean`)
     // console.log("Hello");
 
   }
 
-  excuteHelloWorldBeanServiceWithPathVariable(name) {
-    let basicAuthHeadertring = this.createBasicAuthHttpHeader();
+  excuteHelloWorldBeanServiceWithPathVariable(username) {
 
-    let header: HttpHeaders;
-    header = new HttpHeaders({
-      Authorization: basicAuthHeadertring
-    });
-    return this.http.get<HelloWorldBean>(`http://localhost:8080/hello-world-bean/path-variable/${name}`,
-      {headers: header})
+    // let basicAuthHeadertring = this.createBasicAuthHttpHeader();
+    //
+    // let header: HttpHeaders;
+    // header = new HttpHeaders({
+    //   Authorization: basicAuthHeaderString
+    // });
+
+    return this.http.get<HelloWorldBean>(`${API_URL}/hello-world-bean/path-variable/${username}`
+      // ,{headers: header}
+    )
     // console.log("Hello");
-
   }
 
-  createBasicAuthHttpHeader() {
-    let username = 'rhioda';
-    let password = 'rhioda';
-    let basicAuthHeaderString = 'Basic ' + window.btoa(username + ':' + password);
-    return basicAuthHeaderString;
-  }
+  // createBasicAuthHttpHeader() {
+  //   let username = 'rhioda';
+  //   let password = 'rhioda';
+  //   let basicAuthHeaderString = 'Basic ' + window.btoa(username + ':' + password);
+  //   return basicAuthHeaderString;
+  // }
 
 }
 
 export class HelloWorldBean {
   constructor(
-    // public id : number,
-    // public date: date,
+    public id: number,
+    public targetDate: Date,
     public message: string
   ) {
 
--- todo-ui/src/app/service/http/http-intercepter-basic-auth.service.ts
+++ todo-ui/src/app/service/http/http-intercepter-basic-auth.service.ts
@@ -1,26 +1,50 @@
 import {Injectable} from '@angular/core';
 import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from "@angular/common/http";
 import {Observable} from "rxjs/internal/Observable";
+import {BasicAuthenticationService} from "../basic-authentication.service";
 
 @Injectable({
   providedIn: 'root'
 })
-export class HttpIntercepterBasicAuthService
-  implements HttpInterceptor {
+export class HttpIntercepterBasicAuthService implements HttpInterceptor {
 
-  constructor() {
-  }
+  basicAuthHeaderString:string;
+  username:string;
 
-  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    let username = 'rhioda';
-    let password = 'rhioda';
-    let basicAuthHeaderString = 'Basic ' + window.btoa(username + ':' + password);
+  constructor(
+    private basicAuthenticationService:BasicAuthenticationService
+  ) { }
 
-    req = req.clone({
-      setHeaders: {
-        Authorization: basicAuthHeaderString
-      }
-    })
+  intercept(req:HttpRequest<any>, next:HttpHandler)
+  // : Observable<HttpEvent<any>>
+  {
+
+    // let username = 'rhioda';
+    // let password = 'rhioda';
+    // let basicAuthHeaderString = 'Basic ' + window.btoa(username + ':' + password);
+
+    // this.basicAuthHeaderString = this.basicAuthenticationService.getAuthenticatedToken();
+    // this.username = this.basicAuthenticationService.getAuthenticatedUser()
+
+    let basicAuthHeaderString = this.basicAuthenticationService.getAuthenticatedToken();
+    let username = this.basicAuthenticationService.getAuthenticatedUser()
+
+    console.log('intercept | username : ' + this.username + ' | basicAuthHeaderString : ' + this.basicAuthHeaderString)
+
+    if (basicAuthHeaderString!= null && username!= null) {
+    // if (this.basicAuthHeaderString && this.username) {
+    // if (basicAuthHeaderString && username) {
+
+      console.log('User is logged in --- intercept | username : ' + this.username + ' | basicAuthHeaderString : ' + this.basicAuthHeaderString);
+      req = req.clone({
+        setHeaders: {
+          Authorization: basicAuthHeaderString
+        }
+      })
+    } else {
+      console.log('User is not logged in --- intercept | username : ' + this.username + ' | basicAuthHeaderString : ' + this.basicAuthHeaderString)
+    }
+
     return next.handle(req);
   }
 
